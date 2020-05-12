67249a43df92ba092c2927983b30f8f79db2a829
amateurdullahan <zabrak12@gmail.com>
2020-05-12 23:21:53 +0000

    moved swap and pop in opcode_funcs, deleted unnecessary files

--- opcode_funcs.c
+++ opcode_funcs.c
@@ -1,12 +1,46 @@
 #include "monty.h"
 /**
+ * pop - pop out a stack
+ * stack: the stack to pop out of
+ * line_number: line number for error code
+ *
+ * Return: void
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+  if (*stack != NULL)
+    {
+      *stack = (*stack)->next;
+      (*stack)->prev = NULL;
+    }
+}
+
+/**
+ * swap - swap top two elements of stack
+ * stack: the stack to swap
+ * line_number: line number for error code
+ *
+ * Return: void
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+  int temp = (*stack)->n;
+  (*stack)->n = (*stack)->next->n;
+  (*stack)->next->n = temp;
+
+}
+
+
+/**
 * pall_op - opcode function
 * @stack_head: stack given to print
 *
 * Description: opcode function to print all stack values beginning at top
 * Return: 0 success
 */
-void pall_op(stack_t *stack_head)
+void pall_op(stack_t **stack_head)
 {
 
 	while (stack_head != NULL)
