4234ec378b0ad23057f5084817d851f1b2511963
Jilroge7 <1672@holbertonschool.com>
2020-05-14 22:12:06 +0000

    adding comments advanced

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+void comments(stack_t **stack, unsigned int line_number);
 void parse(char *c, stack_t **stack, unsigned int line_number);
 int _strcmp(char *s1, char *s2);
 void freelist(stack_t **stack);
